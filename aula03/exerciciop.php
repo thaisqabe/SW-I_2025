63238f4887c3d63f2af0a510b9b9833e99c18712
Thaís Abe <159479714+thaisqabe@users.noreply.github.com>
2025-02-27 10:55:25 -0300

    Add files via upload

---
+++ aula03/exerciciop.php
@@ -0,0 +1,8 @@
+<?php
+    $nome= $_POST['caixanome'];
+    $mensa= $_POST['mensagem'];
+
+    echo "Nome digitado: $nome<br>";
+    echo "obrigada pela mensagem!<br> Confira a seguir o que foi escrito: $mensa<br>";
+
+?>
---
+++ aula03/processa01.php
@@ -0,0 +1,8 @@
+<?php
+    $nome= $_GET['cxnome'];
+    $idade= $_GET['cxidade'];
+
+    echo "Nome digitado: $nome<br>";
+    echo "Idade digitada: $idade<br>";
+
+?>
